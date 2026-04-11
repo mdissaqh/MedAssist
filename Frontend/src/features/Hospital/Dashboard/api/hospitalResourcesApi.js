d9de42e65c7e9eeee0a4bf28e9b49b54c8dd79c1
Mohammed Issaqh <mdissaqh2007@gmail.com>
2026-04-11 07:22:03 +0530

    udpated hospital api

--- Frontend/src/features/Hospital/Dashboard/api/hospitalResourcesApi.js
+++ Frontend/src/features/Hospital/Dashboard/api/hospitalResourcesApi.js
@@ -2,12 +2,12 @@ import axiosInstance from '../../../../api/axiosInstance';
 
 // Fetch current data
 export const getHospitalProfileApi = async () => {
-  const response = await axiosInstance.get('/hospital/profile');
+  const response = await axiosInstance.get('/api/hospital/profile');
   return response.data;
 };
 
 // Update data
 export const updateHospitalResourcesApi = async (resourceData) => {
-  const response = await axiosInstance.put('/hospital/resources', resourceData);
+  const response = await axiosInstance.put('/api/hospital/resources', resourceData);
   return response.data;
 };
