637f68526876648e2733535ab3dad2ea6d79b9de
Mohammed Issaqh <mdissaqh2007@gmail.com>
2026-04-11 07:59:34 +0530

    updated

--- Frontend/src/features/Patient/Chat/api/chatApi.js
+++ Frontend/src/features/Patient/Chat/api/chatApi.js
@@ -1,7 +1,7 @@
 import axiosInstance from '../../../../api/axiosInstance';
 
 export const sendChatMessageApi = async (patientId, message, history, location, language) => {
-  const response = await axiosInstance.post('/ai/chat', {
+  const response = await axiosInstance.post('api/ai/chat', {
     patientId, message, history, location, language // Pass language here!
   });
   return response.data;
