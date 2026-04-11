33f4b86636c17de302ad881b552dfc05ff9006d0
Mohammed Issaqh <mdissaqh2007@gmail.com>
2026-04-11 06:36:29 +0530

    Corrected backend api

--- Frontend/src/features/Hospital/Login/api/hospitalLoginApi.js
+++ Frontend/src/features/Hospital/Login/api/hospitalLoginApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://medassist-ufl5.onrender.com';
+const API_URL = 'https://medassist-ufl5.onrender.com/api/auth';
 
 export const loginHospitalApi = async (hospitalId, password) => {
   const response = await axios.post(`${API_URL}/hospital/login`, { hospitalId, password });
