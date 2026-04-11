d6401b6fe450d5bc73aa4b9a383014d2d00acd4f
Mohammed Issaqh <mdissaqh2007@gmail.com>
2026-04-11 08:05:31 +0530

    Finally done

--- Frontend/src/features/Admin/HospitalManagement/api/hospitalApi.js
+++ Frontend/src/features/Admin/HospitalManagement/api/hospitalApi.js
@@ -2,16 +2,16 @@ import axiosInstance from '../../../../api/axiosInstance'; // Import our new sec
 
 // Notice we only need the relative path now, because baseURL is set!
 export const addHospitalApi = async (hospitalData) => {
-  const response = await axiosInstance.post('/admin/hospitals', hospitalData);
+  const response = await axiosInstance.post('api/admin/hospitals', hospitalData);
   return response.data;
 };
 
 export const editHospitalApi = async (id, hospitalData) => {
-  const response = await axiosInstance.put(`/admin/hospitals/${id}`, hospitalData);
+  const response = await axiosInstance.put(`api/admin/hospitals/${id}`, hospitalData);
   return response.data;
 };
 
 export const searchHospitalsApi = async (query) => {
-  const response = await axiosInstance.get(`/admin/hospitals/search?query=${query}`);
+  const response = await axiosInstance.get(`api/admin/hospitals/search?query=${query}`);
   return response.data;
 };
